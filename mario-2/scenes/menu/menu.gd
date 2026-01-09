9e28a46a3a68839e5956ddbfe7e76cc935eb654f
hugogruiz <163063959+hugogruiz@users.noreply.github.com>
2026-01-08 16:28:17 -0800

    Descripción de los cambios

--- mario-2/scenes/menu/menu.gd
+++ mario-2/scenes/menu/menu.gd
@@ -1,15 +1,18 @@
 extends MarginContainer
+var main_level_scene = preload("res://scenes/test_level/test_level.tscn")
 
 
-func _on_start_pressed() -> void:
+func _on_credits_pressed() -> void:
+	pass # Replace with function body.
+
+
+func _on_start_button_pressed() -> void:
+	get_tree().change_scene_to_packed(main_level_scene)
 	print("papu:v")
 	pass # Replace with function body.
 
 
-func _on_finish_pressed() -> void:
+func _on_exit_button_pressed() -> void:
 	print("salir papu")
 	get_tree().quit()
-
-
-func _on_credits_pressed() -> void:
 	pass # Replace with function body.
